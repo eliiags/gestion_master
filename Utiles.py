f7bd56f9335414ebc7ff58e851a0c7e5ba68cb62
Elianni <eaguero@ucm.es>
2020-02-29 19:34:11 +0100

    Update and rename Comprobaciones.py to Utiles.py

--- Utiles.py
+++ Utiles.py
@@ -8,7 +8,8 @@ Created on Wed Feb 26 01:57:35 2020
 
 # Para ocultar el numero 
 import getpass
-
+# Generar numero aleatorio
+import random
 
 """ 
     Hace comprobaciones del numero num
@@ -31,26 +32,55 @@ def compruebaNumero(MIN, MAX, num, oculto):
             if (num >= MIN and num <= MAX):
                 # El valor es correcto y sale del bucle
                 break
+            
             # Si el valor no esta dentro del intervalo
-            else:
-                print("Uy, has introducido un valor incorrecto.")
-                print("Recuerda que tiene que ser un número entre " + str(MIN) + " y " + str(MAX))
-                # Si hay que ocultar el numero
-                if (oculto):
-                    num = getpass.getpass("Inténtalo otra vez :) ")
-                # Si no hay que ocultarlo            
-                else:
-                    num = input("Inténtalo otra vez :) ")
+            print("Uy, has introducido un valor incorrecto.")
+                
         # Si el valor introducido no es un numero
         except:
             print("Uy, el valor que has introducido no es un número. Inténtalo otra vez :) ")
-            if (oculto):
-                num = getpass.getpass("Escribe un número entre " + str(MIN) + " y " + str(MAX) + ": ")
-            else:
-                num = input("Escribe un número entre " + str(MIN) + " y " + str(MAX) + ": ")
 
+    
+        print("Recuerda que tiene que ser un número entre " + str(MIN) + " y " + str(MAX))
+
+        # Si hay que ocultar el numero
+        if (oculto):
+            num = getpass.getpass("Inténtalo otra vez :) ")
+        # Si no hay que ocultarlo            
+        else:
+            num = input("Inténtalo otra vez :) ")
 
     return num # Devuelve el valor correcto
 
+
+def compruebaString(cadena, s1, s2):
+    
+    while (True):
+        if (cadena.upper() == s1):
+            return True
+        elif (cadena.upper() == s2):
+            return False
+        
+        print("Debes escribir '"+ s1 + "' o '" + s2 + "'")
+        cadena = input("¿Quieres una pista? ")
+
+
+def dameNumAleatorio(MIN, MAX):
+    return random.randint(MIN, MAX)
+
+
+def getColor(color):
+    
+    if(color == 'R'):
+        return '\x1b[1;31m'
+    elif(color == 'V'):
+        return '\x1b[1;32m'
+    elif(color == 'A'):
+        return '\x1b[1;33m'
+    
+    # (color == 'B'):
+    return '\x1b[1;37m'
+
+    
 
 """  FIN COMPROBACIONES  """
