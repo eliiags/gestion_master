818096866dd19e298934423ca2b42bf833114381
Elianni <eaguero@ucm.es>
2020-02-29 19:34:57 +0100

    Update NivelJuego.py

--- NivelJuego.py
+++ NivelJuego.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 26 00:53:55 2020
-
-@author: Elianni Aguero, Angelica Guerrero, Cinthya Quintana
+@author: Elianni Aguero, Angelica Guerrero, Cynthia Quintana
 """
 
 
 """
 Clase Nivel
- -  Representa los niveles del juego
+ - Representa los niveles del juego
 """
 class Nivel():
-    def __init__(self, MAX, MIN, intentos):
+    def __init__(self, MIN, MAX, intentos):
         # Numero de intentos del nivel 
         self._n_intentos = intentos
         # Rango Maximo en el cual debe adivinarse el numero
@@ -43,18 +41,5 @@ class Nivel():
         self._MIN = MIN
 
         
-
-class Facil(Nivel):
-    def __init__(self, MAX, MIN, intentos):
-        super().__init__(MAX, MIN, intentos)
-
-class Medio(Nivel):
-    def __init__(self, MAX, MIN, intentos):
-        super().__init__(MAX, MIN, intentos)
-
-class Dificil(Nivel):
-    def __init__(self, MAX, MIN, intentos):
-        super().__init__(MAX, MIN, intentos)
-
 
 """  FIN NIVEL JUEGO  """
